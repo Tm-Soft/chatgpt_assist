881645badba40395fc50d55aae9843d2a0d7a525
kukwonho <kukwonho@gmail.com>
2023-10-01 18:45:29 +0900

    Initial commit

---
+++ app/src/main/java/live/lafi/chatgpt_assist/MainActivity.kt
@@ -0,0 +1,11 @@
+package live.lafi.chatgpt_assist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
